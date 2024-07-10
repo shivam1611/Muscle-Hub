1a9c97b9eda091120997dc49eed927cf957cc8ac
shivam1611 <iamshivamsharma755@gmail.com>
2024-07-10 17:16:16 +0530

    contact page setup

--- src/pages/ContactPage/ContactPage.jsx
+++ src/pages/ContactPage/ContactPage.jsx
@@ -1,6 +1,7 @@
 import Navbar from "../../components/Navbar/Navbar";
 import TrainerCard from "../../components/TrainerCard/TrainerCard";
 import classes from "./ContactPage.module.css";
+import Footer from "../../components/Footer/Footer";
 import { motion } from "framer-motion";
 
 function ContactPage() {
@@ -69,6 +70,71 @@ function ContactPage() {
           />
         ))}
       </div>
+      <div className={classes.map_container}>
+        <div className={classes.map_side}>
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d27711.731606101777!2d78.51186138443691!3d29.74968411813812!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39097b59890b0c29%3A0x8168351f216f51a8!2sR.T.O.%20Kotdwar!5e0!3m2!1sen!2sin!4v1720609572970!5m2!1sen!2sin"
+            allowfullscreen=""
+            loading="lazy"
+            referrerPolicy="no-referrer-when-downgrade"
+            className={classes.map}
+          ></iframe>
+        </div>
+        <div className={classes.form_side}>
+          <form  className={classes.form}>
+            <div className={classes.row}>
+              <div className={classes.cont}>
+                <label htmlFor="name" id="name" className={classes.label}>
+                  Name
+                </label>
+                <input
+                  type="text"
+                  name="name"
+                  className={classes.input_field}
+                  placeholder="Full Name"
+                />
+              </div>
+              <div className={classes.cont}>
+                <label htmlFor="email" id="email" className={classes.label}>
+                  Email Address
+                </label>
+                <input
+                  type="email "
+                  name="email"
+                  className={classes.input_field}
+                  placeholder="Email Address"
+                />
+              </div>
+            </div>
+            <div className={classes.row}>
+              <div className={classes.cont}>
+                <label htmlFor="subject" className={classes.label}>
+                  Subject
+                </label>
+                <input
+                  type="text"
+                  className={classes.input_field}
+                  placeholder="Subject"
+                />
+              </div>
+            </div>
+            <div className={classes.row}>
+              <div className={classes.cont}>
+                <label htmlFor="message" className={classes.label}>
+                  Message
+                </label>
+                <textarea
+                  name="message"
+                  id="message"
+                  placeholder="Message"
+                ></textarea>
+              </div>
+            </div>
+            <button className={classes.btn_lg}>Submit</button>
+          </form>
+        </div>
+      </div>
+      <Footer />
     </motion.div>
   );
 }
